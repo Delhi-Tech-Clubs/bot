321c33059cbf49e4513abcdd2b8ebd0fe8b55258
Atharv M <112397185+AtharvM02222@users.noreply.github.com>
2026-08-25 23:49:35 +0530

    feat: dtc

--- dtc.js
+++ dtc.js
@@ -1,1 +1,24 @@
+require("dotenv").config();
 
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const bot = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+bot.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (["dtc", "Dtc", "DTC"].includes(message.content)) {
+    await message.react("❤️");
+    await message.react("🇩");
+    await message.react("🇹");
+    await message.react("🇨");
+  }
+});
+
+bot.login(process.env.TOKEN);
