c5ed79d009c27b1fc7705995707c48a6546c5d1a
Atharv M <112397185+AtharvM02222@users.noreply.github.com>
2026-08-28 22:41:14 +0530

    feat: link
    Added a new message handler for the '!link' command.

--- dtc.js
+++ dtc.js
@@ -26,6 +26,15 @@ dtc.on("messageCreate", async (message) => {
 
 dtc.on("messageCreate", tune);
 
+dtc.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!link") {
+    message.reply("[GitHub](https://github.com/Delhi-Tech-Clubs)");
+    message.react("dtcc:1542895871610462248");
+  }
+});
+
 cron.schedule("0 9 * * *", () => announce(dtc), { timezone: "Asia/Kolkata" });
 
 http.createServer((req, res) => {
